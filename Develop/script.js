0730534b48999b477f741ad68a3f26d378e3d30b
Connor Stroh <connorstroh@Connors-MacBook-Pro.local>
2022-12-22 14:43:29 -0600

    Format update

--- Develop/script.js
+++ Develop/script.js
@@ -7,18 +7,35 @@ var generatePassword = function () {
   var numeric = "123456789";
   var special = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
 
+  // Promt for password length
+  do {
+    passLength = window.prompt("How many characters would you like? Min of 8 and Max of 128");
+    // creating expression to see if passLength is not a number
+    if (passLength < 8 || passLength > 128) {
+      // alerts uers if entry is not between 8 and 128
+      alert("You must enter in a number between 8 and 128!");
+    }
+  } while (passLength < 8 || passLength > 128);
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+  do {
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+    // prompt for password criteria
+    var promptLower = window.confirm("Would you like to include lowercase letters?")
+    var promptUpper = window.confirm("Would you like to include uppercase letters?")
+    var promptNum = window.confirm("Would you like to include numbers?")
+    var promptSpecial = window.confirm("Would you like to include special characters?")
 
-  passwordText.value = password;
+    // Get references to the #generate element
+    var generateBtn = document.querySelector("#generate");
 
-}
+    // Write password to the #password input
+    function writePassword() {
+      var password = generatePassword();
+      var passwordText = document.querySelector("#password");
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+      passwordText.value = password;
+
+    }
+
+    // Add event listener to generate button
+    generateBtn.addEventListener("click", writePassword);
