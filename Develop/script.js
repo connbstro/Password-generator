c30c8aee003f462f635ea4059cf88fac26ca4bb2
Connor Stroh <connorstroh@Connors-MacBook-Pro.local>
2022-12-22 18:18:12 -0600

    format changes

--- Develop/script.js
+++ Develop/script.js
@@ -5,67 +5,66 @@ let generatePassword = function () {
   let lowercase = "abcdefghijklmnnopqrstuvwxyz";
   let uppercase = "ABCDEFGHIJKLMNNOPQRSTUVWXYZ";
   let numeric = "123456789";
-  let special = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+  let special = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
   let newPass = "";
 
   // Set password length
   do {
-    passLength = window.prompt("Set number of characters: Min of 8 and Max of 128");
+    passLength = window.prompt(
+      "Set number of characters: Min of 8 and Max of 128"
+    );
     if (passLength < 8 || passLength > 128) {
       alert("Must enter in a number between 8 and 128");
     }
   } while (passLength < 8 || passLength > 128);
 
+  do {
+    // set password criteria
+    let promptLower = window.confirm(
+      "generate password with lowercase letters?"
+    );
+    let promptUpper = window.confirm(
+      "generate password with uppercase letters?"
+    );
+    let promptNum = window.confirm("generate password with numbers?");
+    let promptSpecial = window.confirm(
+      "generate password with special characters?"
+    );
 
-do {
+    // Check prompt responses
+    if (promptLower) {
+      charSet += lowercase;
+    }
+    if (promptUpper) {
+      charSet += uppercase;
+    }
 
-  // set password criteria
-  let promptLower = window.confirm("generate password with lowercase letters?")
-  let promptUpper = window.confirm("generate password with uppercase letters?")
-  let promptNum = window.confirm("generate password with numbers?")
-  let promptSpecial = window.confirm("generate password with special characters?")
+    if (promptNum) {
+      charSet += numeric;
+    }
 
-  // Check prompt responses
-  if (promptLower) {
-    charSet += lowercase
-  }
-  if (promptUpper) {
-    charSet += uppercase
-  }
+    if (promptSpecial) {
+      charSet += special;
+    }
+  } while (charSet == null);
 
-  if (promptNum) {
-    charSet += numeric
-  }
-
-  if (promptSpecial) {
-    charSet += special
-  }
-
-
-  } while (charSet == null); 
-  
   // Get password length, set password based on prompts
   for (let i = 0; i < passLength; i++) {
-    newPass += charSet[Math.floor(Math.random() * charSet.length)]
-  } 
-  // console.log(newPass); 
+    newPass += charSet[Math.floor(Math.random() * charSet.length)];
+  }
+  // console.log(newPass);
   return newPass;
 };
-  
 
-  // Write password to the #password input
-  function writePassword() {
-    let password = generatePassword();
-    let passwordText = document.querySelector("#password");
+// Write password to the #password input
+function writePassword() {
+  let password = generatePassword();
+  let passwordText = document.querySelector("#password");
 
-    passwordText.value = password;
-    // console.log(password);
+  passwordText.value = password;
+  // console.log(password);
+}
 
-  }
-
-  //   // Add event listener to generate button
+//   // Add event listener to generate button
 let generateBtn = document.querySelector("#generate");
 generateBtn.addEventListener("click", writePassword);
-
-
-
